8b1bc157901efdfaec02050fb49af39335436567
Ahmad Ayob <ahmadayouba2003@gmail.com>
2025-11-26 18:21:53 +0300

    Blade Templating Engine Module

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout title="Contact">
+    <h2>Contact Page</h2>
+    
+</x-layout>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <h2>welcome to Home Page</h2>
+    <ul>
+        <li>Jobs of the day</li>
+        <li>Jobs of the week</li>
+    </ul>
+</x-layout>
