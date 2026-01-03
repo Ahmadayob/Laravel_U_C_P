10c6107cdff44e3d9437cae228553388a9ab0a4e
Ahmad Ayob <ahmadayouba2003@gmail.com>
2026-01-03 16:24:58 +0300

    Fix indentation in create.blade.php

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -10,22 +10,24 @@
                     <div class="sm:col-span-3">
                         <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                         <div class="mt-2">
-                            <input id="title" value="{{ old('title')}}" type="text" name="title" autocomplete="given-name"
+                            <input id="title" value="{{ old('title')}}" type="text" name="title"
+                                autocomplete="given-name"
                                 class="{{$errors->has('title') ? 'border-red-500' : 'outline-gray-300'}} block w-full rounded-md border-2 border-gray-300 bg-white px-3 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 sm:text-sm/6" />
                         </div>
                         @error('title')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                            <span class="text-red-500 text-sm">{{ $message }}</span>
                         @enderror
                     </div>
 
                     <div class="sm:col-span-3">
                         <label for="author" class="block text-sm/6 font-medium text-gray-900">Author</label>
                         <div class="mt-2">
-                            <input id="author" value="{{ old('author')}}" type="text" name="author" autocomplete="family-name"
+                            <input id="author" value="{{ old('author')}}" type="text" name="author"
+                                autocomplete="family-name"
                                 class="{{$errors->has('author') ? 'border-red-500' : 'outline-gray-300'}} block w-full rounded-md border-2 border-gray-300 bg-white px-3 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 sm:text-sm/6" />
                         </div>
                         @error('author')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                            <span class="text-red-500 text-sm">{{ $message }}</span>
                         @enderror
                     </div>
 
@@ -38,7 +40,7 @@
                         </div>
                         <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about the artical.</p>
                         @error('body')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                            <span class="text-red-500 text-sm">{{ $message }}</span>
                         @enderror
                     </div>
 
